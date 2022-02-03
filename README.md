855f9c75b22d3230a41bd03281bea699151d67b1
ChienVM <chien_vm@detomo.co.jp>
2022-02-03 18:47:13 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# face-detection
+# Face-detection-api
+
+## Language: 
+Python 3.8
+
+## Environment: 
+```
+pip intsll -r requirements.txt
+```
+
+## Run: 
+```
+python main.py
+```
+
+### Request
+```
+curl -X 'POST' \
+      'http://127.0.0.1:8000/face_detect?threshold=0.1' \
+      -H 'accept: application/json' \
+      -H 'Content-Type: multipart/form-data' \
+      -F 'file=@test.jpg;type=image/jpeg'
+```
+
+### Response
+```
+{
+  "distances": [
+    0.1214
+  ],
+  "detect_close_face": 1,
+  "detect_face": 7
+}
+```
---
+++ main.py
@@ -0,0 +1,72 @@
+import warnings
+import uvicorn
+warnings.simplefilter("ignore", UserWarning)
+from fastapi import FastAPI, UploadFile, File
+from fastapi.logger import logger
+from mtcnn import MTCNN
+from PIL import Image
+from utils.detect_face import calculate_distance
+
+app = FastAPI(
+    title="AI Model Face Detection",
+    version="0.0.1"
+)
+device = "cpu"
+mtcnn = MTCNN(
+        image_size=256, margin=0, min_face_size=20,
+        thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+        device=device
+)
+
+logger.info("Loaded models successfully")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/face_detect")
+def predict(file: UploadFile = File(...),  threshold: float = 0.1):
+    """ Detect face from image
+
+           Parameters:
+           ----------------
+
+           file: upload file image
+
+               ALLOWED_EXTENSION = {'jpg', 'jpeg'}
+
+           threshold: float
+
+               threshold for distances between faces and input image
+
+           Returns:
+           ----------------
+           distances: list
+
+               list of distances between faces and input image
+
+           detect_close_face: interger
+
+               number of close faces detected
+
+           detect_face: interger
+
+               number of faces detected
+
+    """
+    threshold = threshold
+    contents = file.file
+    image = Image.open(contents)
+    boxes, probs = mtcnn.detect(image, landmarks=False)
+    distances = calculate_distance(image, boxes.tolist(), threshold)
+    return {
+        "distances": distances,
+        "detect_close_face": len(distances),
+        "detect_face": len(probs.tolist())
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True, debug=True)
